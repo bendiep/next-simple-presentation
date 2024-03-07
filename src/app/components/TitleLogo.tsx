eb181c8ee92a01acd063a332e75ad3d759a7e941
Ben Diep <ben_diep@hotmail.com>
2024-03-07 22:30:07 +1100

    update

--- src/app/components/TitleLogo.tsx
+++ src/app/components/TitleLogo.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 
-// import logo from "@/images/example-logo.webp";
-// import logo from "@/images/fd-logo.jpeg";
 import logo from "@/images/fd-logo.gif";
 
-const TitleLogo = ({ title }: { title: string }) => {
+const TitleLogo = () => {
   return (
     <div className="text-center mb-8">
       <Image className="rounded-lg" src={logo} alt="Logo" width={300} />
-      <h1 className="text-5xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        {title}
+      <h1 className="mt-20 mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <mark className="px-3 py-1 text-white bg-sky-500 rounded dark:bg-sky-500">
+          Ways
+        </mark>{" "}
+        of{" "}
+        <mark className="px-3 py-1 text-white bg-sky-500 rounded dark:bg-sky-500">
+          Working
+        </mark>{" "}
       </h1>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import TitleLogo from "./components/TitleLogo";
 import PresenterCard from "./components/PresenterCard";
 
-import presenterOne from "@/images/example-photo-1.jpeg";
-import presenterTwo from "@/images/example-photo-2.jpeg";
-import presenterThree from "@/images/example-photo-3.jpeg";
+import presenterOne from "@/images/example-photo-1.jpg";
+import presenterTwo from "@/images/example-photo-2.jpg";
+import presenterThree from "@/images/example-photo-3.jpg";
+import presenterFour from "@/images/example-photo-4.jpg";
 
 export default function Home() {
   const presenters = [
@@ -19,17 +20,23 @@ export default function Home() {
       name: "Presenter Two",
       topic: "Topic Title Two",
     },
+    // {
+    //   id: 3,
+    //   src: presenterThree,
+    //   name: "Presenter Three",
+    //   topic: "Topic Title Three",
+    // },
     {
-      id: 3,
-      src: presenterThree,
-      name: "Presenter Two",
-      topic: "Topic Title Three",
+      id: 4,
+      src: presenterFour,
+      name: "Presenter Four",
+      topic: "Topic Title Four",
     },
   ];
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <TitleLogo title="Presentation Title" />
+      <TitleLogo />
       <div className="flex flex-wrap justify-center">
         {presenters.map((presenter) => (
           <PresenterCard key={presenter.id} {...presenter} />
