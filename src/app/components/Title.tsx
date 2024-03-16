3622a6b6fac20327fe4a598c76baf693a9e531b0
Ben Diep <ben_diep@hotmail.com>
2024-03-16 23:43:30 +1100

    updates

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -5,8 +5,8 @@ export const Title = ({ date }: { date: string }) => {
         {date}
       </p>
       <h1 className="mb-4 mt-0 text-6xl font-extrabold leading-none tracking-tight text-gray-900">
-        <mark className="rounded bg-sky-500 px-3 py-1 text-white">Ways</mark> of{' '}
-        <mark className="rounded bg-sky-500 px-3 py-1 text-white">Working</mark>{' '}
+        <mark className="rounded bg-sky-500 px-4 py-1 text-white">Ways</mark> of{' '}
+        <mark className="rounded bg-sky-500 px-4 py-1 text-white">Working</mark>{' '}
       </h1>
     </div>
   )
