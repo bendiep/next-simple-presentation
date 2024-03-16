651c4d1a6afccaea81792d33b8c18add405ac06f
Ben Diep <ben_diep@hotmail.com>
2024-03-17 00:49:40 +1100

    Update Timer.tsx

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -35,68 +35,45 @@ export const Timer = () => {
       </div>
       <p>{isRunning ? 'Running' : 'Not running'}</p>
       <div>
-        <input
-          type="number"
-          value={inputMinutes}
-          onChange={(e) => setInputMinutes(parseInt(e.target.value))}
-          placeholder="mins"
-          style={{
-            width: '5rem',
-            marginRight: '10px',
-            padding: '10px',
-            borderRadius: '5px',
-            border: '1px solid #ccc',
-            outline: 'none',
-          }}
-        />
-        <input
-          type="number"
-          value={inputSeconds}
-          onChange={(e) => setInputSeconds(parseInt(e.target.value))}
-          placeholder="sec"
-          style={{
-            width: '5rem',
-            padding: '10px',
-            borderRadius: '5px',
-            border: '1px solid #ccc',
-            outline: 'none',
-          }}
-        />
-      </div>
-      <button onClick={startTimer}>Start</button>
-      <br />
-      <button onClick={resetTimer}>Reset</button>
-
-      {/* <div>
-        <label
-          htmlFor="price"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          Timer
-        </label>
-        <div className="relative mt-2 rounded-md shadow-sm w-100">
+        <div className="w-100 relative mt-2 rounded-md shadow-sm">
           <input
-            type="text"
-            name="price"
-            id="price"
+            type="number"
+            name="timer-minutes"
+            id="timer-minutes"
             className="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             placeholder="0"
-            aria-describedby="price-currency"
+            aria-describedby="timer-minutes"
+            onChange={(e) => setInputMinutes(parseInt(e.target.value))}
           />
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <span className="text-gray-500 sm:text-sm" id="price-currency">
-              mins
+            <span className="text-gray-500 sm:text-sm" id="timer-minutes">
+              min
             </span>
           </div>
         </div>
+        <div className="w-100 relative mt-2 rounded-md shadow-sm">
+          <input
+            type="number"
+            name="timer-seconds"
+            id="timer-seconds"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="0"
+            aria-describedby="timer-seconds"
+            onChange={(e) => setInputSeconds(parseInt(e.target.value))}
+          />
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <span className="text-gray-500 sm:text-sm" id="timer-seconds">
+              sec
+            </span>
+          </div>
+        </div>
+      </div>
 
-        <label
-          htmlFor="price"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          Show Timer / Hide Timer
-        </label>
-      </div> */}
+      <br />
+      <br />
+      <button onClick={startTimer}>Start</button>
+      <br />
+      <button onClick={resetTimer}>Reset</button>
     </div>
   )
 }
