1b4a33b8c05c6068f28371c59591a308abe7743c
Ben Diep <ben_diep@hotmail.com>
2024-03-16 11:40:50 +1100

    update

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,14 +1,9 @@
 const Title = () => {
   return (
     <div className="mb-8 text-center">
-      <h1 className="mb-16 mt-20 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        <mark className="rounded bg-sky-500 px-3 py-1 text-white dark:bg-sky-500">
-          Ways
-        </mark>{' '}
-        of{' '}
-        <mark className="rounded bg-sky-500 px-3 py-1 text-white dark:bg-sky-500">
-          Working
-        </mark>{' '}
+      <h1 className="mb-16 mt-20 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        <mark className="rounded bg-sky-500 px-3 py-1 text-white">Ways</mark> of{' '}
+        <mark className="rounded bg-sky-500 px-3 py-1 text-white">Working</mark>{' '}
       </h1>
     </div>
   )
