64d43f5b37a29bdff61e4842a3a5ac09cee370c4
Ben Diep <ben_diep@hotmail.com>
2024-03-17 01:28:38 +1100

    Update Title.tsx

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,10 +1,10 @@
 export const Header = ({ date }: { date: string }) => {
   return (
-    <div className="mb-8 text-center">
-      <p className="absolute left-0 top-0 p-2 text-6xl font-bold text-zinc-200">
+    <div className="text-center md:mb-8">
+      <p className="p-2 text-left text-6xl font-bold text-zinc-200 md:absolute md:left-0 md:top-0">
         {date}
       </p>
-      <h1 className="mb-4 mt-0 text-6xl font-extrabold leading-normal tracking-tight text-gray-900 sm:mt-20">
+      <h1 className="mb-4 mt-0 text-6xl font-extrabold leading-normal tracking-tight text-gray-900">
         <mark className="rounded bg-sky-500 px-4 py-1 text-white">Ways</mark>{' '}
         <span className="">of</span>{' '}
         <mark className="rounded bg-sky-500 px-4 py-1 text-white">Working</mark>{' '}
