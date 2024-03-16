6e3a086258b5e0d4232c7829c49975915e5c851d
Ben Diep <ben_diep@hotmail.com>
2024-03-17 02:31:42 +1100

    Update Timer input values

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -49,7 +49,7 @@ export const Timer = () => {
             id="timer-minutes"
             className="w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
             placeholder="0"
-            aria-describedby="timer-minutes"
+            value={inputMinutes}
             onChange={(e) => setInputMinutes(parseInt(e.target.value))}
           />
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
@@ -65,7 +65,7 @@ export const Timer = () => {
             id="timer-seconds"
             className="w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
             placeholder="0"
-            aria-describedby="timer-seconds"
+            value={inputSeconds}
             onChange={(e) => setInputSeconds(parseInt(e.target.value))}
           />
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
@@ -87,7 +87,6 @@ export const Timer = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="#ffffff"
-            aria-hidden="true"
           >
             <path
               d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z"
@@ -107,7 +106,6 @@ export const Timer = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="#ffffff"
-            aria-hidden="true"
           >
             <path
               d="M12 2.99982C16.9706 2.99982 21 7.02925 21 11.9998C21 16.9704 16.9706 20.9998 12 20.9998C7.02944 20.9998 3 16.9704 3 11.9998C3 9.17255 4.30367 6.64977 6.34267 4.99982"
