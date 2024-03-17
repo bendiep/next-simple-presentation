caa2390edb34d58a05cdcc69cd0d6e0b584c1cd4
Ben Diep <ben_diep@hotmail.com>
2024-03-17 22:38:51 +1100

    Update Title component styling

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -4,10 +4,12 @@ export const Header = ({ date }: { date: string }) => {
       <p className="p-2 text-left text-6xl font-bold text-zinc-200 md:absolute md:left-0 md:top-0">
         {date}
       </p>
-      <h1 className="mb-4 mt-0 text-6xl font-extrabold leading-normal tracking-tight text-gray-900">
-        <mark className="rounded bg-sky-500 px-4 py-1 text-white">Ways</mark>{' '}
-        <span className="">of</span>{' '}
-        <mark className="rounded bg-sky-500 px-4 py-1 text-white">Working</mark>{' '}
+      <h1 className="mb-4 mt-0 font-sans text-6xl font-extrabold leading-normal tracking-tight text-gray-900">
+        <mark className="rounded bg-sky-500 px-4 py-1 text-zinc-50">Ways</mark>{' '}
+        <span className="text-sky-500">of</span>{' '}
+        <mark className="rounded bg-sky-500 px-4 py-1 text-zinc-50">
+          Working
+        </mark>{' '}
       </h1>
     </div>
   )
