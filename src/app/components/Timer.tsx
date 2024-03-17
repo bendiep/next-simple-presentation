5667730b69c5468e837c812ca818b1b2c9769429
Ben Diep <ben_diep@hotmail.com>
2024-03-17 19:02:52 +1100

    Update Timer.tsx

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -3,6 +3,46 @@
 import React, { useState } from 'react'
 import { useTimer } from 'react-timer-hook'
 
+function StartIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      className="h-5 w-5"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ffffff"
+      {...props}
+    >
+      <path
+        d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z"
+        stroke="#ffffff"
+        strokeWidth="2.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+function ResetIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="#ffffff">
+      <path
+        d="M12 2.99982C16.9706 2.99982 21 7.02925 21 11.9998C21 16.9704 16.9706 20.9998 12 20.9998C7.02944 20.9998 3 16.9704 3 11.9998C3 9.17255 4.30367 6.64977 6.34267 4.99982"
+        stroke="#ffffff"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>{' '}
+      <path
+        d="M3 4.49982H7V8.49982"
+        stroke="#ffffff"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
 export const Timer = () => {
   const [showTimer, setShowTimer] = useState(true)
   const [inputMinutes, setInputMinutes] = useState(0)
@@ -63,9 +103,6 @@ export const Timer = () => {
               min="0"
               value={inputMinutes}
               onChange={(e) => {
-                if (isNaN(parseInt(e.target.value))) {
-                  return
-                }
                 setInputMinutes(parseInt(e.target.value ? e.target.value : ''))
               }}
             />
@@ -85,10 +122,7 @@ export const Timer = () => {
               min="0"
               value={inputSeconds}
               onChange={(e) => {
-                if (isNaN(parseInt(e.target.value))) {
-                  return
-                }
-                setInputSeconds(parseInt(e.target.value))
+                setInputSeconds(parseInt(e.target.value ? e.target.value : ''))
               }}
             />
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
@@ -101,49 +135,17 @@ export const Timer = () => {
         <div className="mt-4">
           <button
             type="button"
-            className="hover:bg-blue-70 m-1 rounded-full bg-blue-500 p-2 text-white"
+            className="m-1 rounded-full bg-blue-500 p-2 text-white hover:bg-blue-700"
             onClick={startTimer}
           >
-            <svg
-              className="h-5 w-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#ffffff"
-            >
-              <path
-                d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z"
-                stroke="#ffffff"
-                strokeWidth="2.5"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <StartIcon />
           </button>
           <button
             type="button"
             className="m-1 rounded-full bg-red-500 p-2 text-white hover:bg-red-700"
             onClick={resetTimer}
           >
-            <svg
-              className="h-5 w-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#ffffff"
-            >
-              <path
-                d="M12 2.99982C16.9706 2.99982 21 7.02925 21 11.9998C21 16.9704 16.9706 20.9998 12 20.9998C7.02944 20.9998 3 16.9704 3 11.9998C3 9.17255 4.30367 6.64977 6.34267 4.99982"
-                stroke="#ffffff"
-                strokeWidth="2.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>{' '}
-              <path
-                d="M3 4.49982H7V8.49982"
-                stroke="#ffffff"
-                strokeWidth="2.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <ResetIcon />
           </button>
         </div>
       </div>
