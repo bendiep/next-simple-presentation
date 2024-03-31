0bc26939b8807360a556bcf80b5060d82a6eca73
Ben Diep <ben_diep@hotmail.com>
2024-04-01 01:17:29 +1100

    Add Title component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header'
+import { Title } from './components/Title'
 import { PresenterCard } from './components/PresenterCard'
 import { Timer } from './components/Timer'
 import { Footer } from './components/Footer'
@@ -6,7 +7,6 @@ import { Footer } from './components/Footer'
 import presenterOne from '@/images/example-photo-1.jpeg'
 import presenterTwo from '@/images/example-photo-2.jpeg'
 import presenterThree from '@/images/example-photo-3.jpeg'
-import { Title } from './components/Title'
 
 export default function Home() {
   const date = new Date().toLocaleDateString('en-AU', {
@@ -39,7 +39,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-zinc-900">
       <Header date={date.toUpperCase()} />
       <Title />
-      <h2 className="truncate text-3xl font-bold leading-7 tracking-tight text-gray-900 md:text-4xl dark:text-zinc-100">
+      <h2 className="truncate text-3xl font-bold leading-7 tracking-tight text-gray-700 md:text-4xl dark:text-zinc-100">
         featuring
       </h2>
       <div className="flex flex-wrap justify-center">
