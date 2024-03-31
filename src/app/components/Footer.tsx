38a728b7e47a77cb75c88868a33fd5a21475d4c7
Ben Diep <ben_diep@hotmail.com>
2024-04-01 01:13:14 +1100

    Refactor Footer component to improve layout and add dark mode support

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,9 +4,14 @@ import logo from '@/images/logo.png'
 
 export const Footer = () => {
   return (
-    <footer className="mb-16 mt-16 text-center">
-      <div className="p-2 md:absolute md:bottom-0 md:right-0">
-        <Image className="rounded-lg p-2" src={logo} alt="Logo" width={250} />
+    <footer className="mb-16 mt-16">
+      <div className="absolute bottom-0 right-0 p-2">
+        <Image
+          className="rounded-lg p-2 dark:grayscale"
+          src={logo}
+          alt="Logo"
+          width={250}
+        />
       </div>
     </footer>
   )
